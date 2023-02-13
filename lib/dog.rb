423843878578ef6377c802b1dc13f612c8bb9a11
prince475 <danielmwai475@gmail.com>
2023-02-13 11:25:10 +0300

    Completed Lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Dog
 
+  attr_reader :name, :breed
+  attr_writer :name, :breed
+
+  def dog_name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def dog_breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+
 end
+
+n1=Dog.new
+n1.dog_name = 'Fido'
+n1.dog_breed = 'German Shepherd'
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
 
+
+  attr_reader :name, :job
+  attr_writer :name, :job
+
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def person_job=(job)
+    @job = job
+  end
+  def breed
+    @breed
+  end
+
 end
+
+n1=Person.new
+n1.name = 'prince'
+n1.person_job = 'Engineer'
+
